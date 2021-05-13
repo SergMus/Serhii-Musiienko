b3f0f8f5448093fe73f84d2dd0c3f5b2ef88ed88
Serg <umt1@ukr.net>
2021-05-13 14:19:24 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,167 @@
+window.onload = function () {
+  const cartItems = {};
+  const cartSum = {};
+
+  const items = document.getElementsByClassName("product-box__item");
+
+  const btns = document.querySelectorAll(".product-box__btn");
+  for (let btn of btns) {
+    btn.addEventListener("click", addToCart);
+  }
+
+  document
+    .getElementById("dishes-select")
+    .addEventListener("change", filterByCategory);
+
+  document
+    .getElementById("price-select")
+    .addEventListener("change", filterByPrice);
+
+  document.querySelector(".btn-check").onclick = function checkout() {
+    const modal = document.createElement("div");
+    modal.className = "open";
+    modal.style.cssText =
+      "display: block; position: fixed; z-index: 1; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgba(0,0,0,0.6); z-index: 1000;";
+    const wrapper = document.createElement("div");
+    wrapper.style.cssText =
+      "border-radius: 5px; background-color: #fefefe; margin: 15% auto; max-width: 500px; padding: 50px 80px; border: 1px solid #888; z-index: 99999;";
+    const form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("onsubmit", "alert('Благодарим Вас за покупки!')");
+    const name = document.createElement("input");
+    name.setAttribute("type", "text");
+    name.required = true;
+    name.style.cssText =
+      "width: 100%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box; font-size: 20px";
+    name.setAttribute("placeholder", "Имя");
+    const email = document.createElement("input");
+    email.setAttribute("type", "text");
+    email.required = true;
+    email.style.cssText =
+      "width: 100%; padding: 12px 20px; margin: 8px 0; box-sizing: border-box; font-size: 20px";
+    email.setAttribute("placeholder", "e-mail");
+    const button = document.createElement("button");
+    button.setAttribute("type", "submit");
+    button.style.cssText =
+      "padding: 6px 14px; margin-top: 8px; box-sizing: border-box;";
+    const text = document.createTextNode("Отправить");
+    button.appendChild(text);
+    form.append(name, email, button);
+    wrapper.append(form);
+    modal.append(wrapper);
+    document.body.appendChild(modal);
+    modal.onclick = (e) => {
+      if (e.target.classList.contains("open")) {
+        modal.style.display = "none";
+      }
+    };
+  };
+
+  function addToCart(e) {
+    let val = 1;
+    if (
+      cartItems.hasOwnProperty(
+        e.target.parentElement.parentElement.children[0].innerText
+      )
+    ) {
+      cartItems[
+        e.target.parentElement.parentElement.children[0].innerText
+      ] += 1;
+      cartSum[e.target.parentElement.parentElement.children[0].innerText] *= 2;
+    } else {
+      let name = e.target.parentElement.parentElement.children[0].innerText;
+      cartItems[name] = val;
+      cartSum[name] = +e.target.parentElement.parentElement.dataset.price;
+    }
+
+    showCart();
+  }
+
+  function showCart() {
+    let quantity = 0;
+    let sum = 0;
+
+    for (let i = 0; i < Object.keys(cartItems).length; i++) {
+      quantity = Object.values(cartItems).reduce((prev, curr) => prev + curr);
+    }
+    for (let i = 0; i < Object.keys(cartSum).length; i++) {
+      sum = Object.values(cartSum).reduce((prev, curr) => prev + curr);
+    }
+
+    document.getElementById("quantity").innerHTML = quantity;
+    document.getElementById("sum").innerHTML = sum;
+  }
+
+  function filterByCategory() {
+    let select = document
+      .getElementById("dishes-select")
+      .getElementsByTagName("option");
+
+    for (let i = 0; i < select.length; i++) {
+      if (select[i].selected && select[i].value === "0") {
+        [...items].map((item) => (item.style.display = "block"));
+      }
+      if (select[i].selected && select[i].value === "1") {
+        [...items].map((item) => {
+          return item.dataset.category != "breakfast"
+            ? (item.style.display = "none")
+            : (item.style.display = "block");
+        });
+      }
+      if (select[i].selected && select[i].value === "2") {
+        [...items].map((item) => {
+          return item.dataset.category != "starters"
+            ? (item.style.display = "none")
+            : (item.style.display = "block");
+        });
+      }
+      if (select[i].selected && select[i].value === "3") {
+        [...items].map((item) => {
+          return item.dataset.category != "sides"
+            ? (item.style.display = "none")
+            : (item.style.display = "block");
+        });
+      }
+    }
+  }
+
+  function filterByPrice() {
+    let select = document
+      .getElementById("price-select")
+      .getElementsByTagName("option");
+
+    for (let i = 0; i < select.length; i++) {
+      if (select[i].selected && select[i].value === "0") {
+        [...items].map((item) => (item.style.display = "block"));
+      }
+      if (select[i].selected && select[i].value === "30") {
+        [...items].map((item) => {
+          return +item.dataset.price < 30
+            ? (item.style.display = "block")
+            : (item.style.display = "none");
+        });
+      }
+      if (select[i].selected && select[i].value === "50") {
+        [...items].map((item) => {
+          return +item.dataset.price < 50
+            ? (item.style.display = "block")
+            : (item.style.display = "none");
+        });
+      }
+      if (select[i].selected && select[i].value === "100") {
+        [...items].map((item) => {
+          return +item.dataset.price < 100
+            ? (item.style.display = "block")
+            : (item.style.display = "none");
+        });
+      }
+      if (select[i].selected && select[i].value === "150") {
+        [...items].map((item) => {
+          return +item.dataset.price < 150
+            ? (item.style.display = "block")
+            : (item.style.display = "none");
+        });
+      }
+    }
+  }
+};
